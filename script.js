5235a30c4bb25952c151dc5b83e4a982c5a2ce4e
Roxana <ciobanuroxana2998@gmail.com>
2021-09-23 00:57:26 +0300

    worked at the logic of the game

--- script.js
+++ script.js
@@ -1,13 +1,85 @@
 const container = document.querySelector(".container");
-console.log(container);
+const clearButton = document.querySelector(".clear-button");
+const changeInput = document.querySelector(".change-input");
+const colorButton = document.querySelector(".color-button");
+const colorPalette = document.querySelector(".change-color");
+const blackButton = document.querySelector(".black-button");
+const eraseButton = document.querySelector(".erase-button");
 
-function createCells(cellsNumber) {
-    for(i = 0; i < cellsNumber; i++) {
-        let cell = document. createElement("div");
-        cell.className = "cell-container";
-        container.appendChild(cell);
-    }
+let color = "black";
+changeInput.value = 16;
+
+function clearCells() {
+  let cells = document.querySelectorAll(".cell");
+  for (i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = "white";
+  }
+
+  //   let cells2 = [...document.querySelectorAll(".cell")];
+  //   for (let cell of cells2) {
+  //     cell.style.backgroundColor = "white";
+  //   }
+}
+
+function clearGrid(cellsNumber) {
+  for (i = 0; i < cellsNumber; i++) {
+    container.innerHTML = "";
+  }
+}
+
+function getColor() {
+  if (color === "random") {
+    let random = () => Math.floor(Math.random() * 256);
+
+    let red = random();
+    let green = random();
+    let blue = random();
+
+    return `rgb(${red},${green},${blue})`;
+  } else {
+    return color;
+  }
 }
 
 
-createCells(256);
+function createCells(cellsNumber) {
+  for (i = 0; i < cellsNumber ** 2; i++) {
+    let newCell = document.createElement("div");
+    newCell.className = "cell";
+    container.appendChild(newCell);
+    newCell.addEventListener("mouseover", function () {
+      newCell.style.backgroundColor = getColor();
+      console.log(newCell.style.backgroundColor);
+    });
+  }
+}
+
+createCells(16);
+
+clearButton.addEventListener("click", function () {
+    clearCells();
+  });
+
+blackButton.addEventListener("click", function() {
+    color = "black";
+});
+
+colorButton.addEventListener("click", function () {
+    color = "random";
+  });
+  
+colorPalette.addEventListener("change", function () {
+    color = colorPalette.value;
+  });
+  
+
+eraseButton.addEventListener("click", function() {
+      color = "white";
+  });
+  
+  changeInput.addEventListener("change", function () {
+    let userInput = changeInput.value;
+    clearGrid(container.children.length);
+    container.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+    createCells(userInput);
+  });
